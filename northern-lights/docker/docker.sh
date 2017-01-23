32b090a63d37444c340b5e0e233fe1d6d23b85c7
RogrWhitakr <bennoosterholt@gmail.com>
2017-01-23 18:39:58 +0100

    test

---
+++ northern-lights/docker/docker.sh
@@ -0,0 +1,24 @@
+# bin/sh
+#variabes
+container=docker.io/nginx:latest
+clear
+echo
+echo Docker Prozesse : Command [docker ps]
+echo
+docker ps
+
+
+echo
+echo vorhandenene docker images : Command [docker images]
+echo
+docker images
+read -n1 -r -p "Press key to continue..." key
+
+echo
+echo Docker run Options : -it [interactive] -d [detached] --rm --help -p [port-forwarding 8080:8090 \| host:container] 
+echo
+
+#docker run --rm $container
+docker run -d $container
+read -n1 -r -p "Press key to continue..." key
+
