7fc4303a606e3487014148f2536a0ee624785701
rogrwhitakr <bennoosterholt@gmail.com>
2017-03-10 08:38:18 +0100

    nothing yet, needs work

--- coreutils/loop-through-variable.sh
+++ coreutils/loop-through-variable.sh
@@ -5,3 +5,17 @@ NOC='\033[0m'
 RED='\033[0;31m'
 YELLOW='\033[1;33m'
 GREEN='\033[0;32m'
+
+SQL="select claim_id from claim;"
+
+SQL=$(sed -e "s/claim/setting/g" <<< $SQL)
+
+echo -e "${RED}SQL${NOC}"
+
+VARIABLES=( one two three four )
+
+for VARIABLE in $( VARIABLES[@] ); 
+do
+	echo -e "$VARIABLE\n"
+	sleep 2
+done
