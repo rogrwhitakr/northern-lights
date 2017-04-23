c9e75fecc4c2309fc1237840214683e907b4d5d4
admin <admin@serenity.northernlights>
2017-04-23 14:13:27 +0200

    gitk test

--- fedora/setup-myscripts.sh
+++ fedora/setup-myscripts.sh
@@ -4,14 +4,3 @@ cd ~/
 
 mkdir MyScripts
 
-cd MyScripts
-read 
-servicename=update-linux
-
-touch /etc/systemd/system/$servicename.service
-chmod 664 /etc/systemd/system/$servicename.service
-
-cd /etc/systemd/system/
-
-systemctl daemon-reload
-systemctl start $servicename.service
