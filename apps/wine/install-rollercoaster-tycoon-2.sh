c66c69923ece05139b2cb42070049cd4dd7f1ea9
rogrwhitakr <bennoosterholt@gmail.com>
2017-06-16 17:26:00 +0200

    rct in docker setup, needs to be tested

--- apps/wine/install-rollercoaster-tycoon-2.sh
+++ apps/wine/install-rollercoaster-tycoon-2.sh
@@ -11,7 +11,7 @@
 #################
 
 printline() {
-	echo [-------------------------------------------------]
+	echo [---------------------------------------------------------------------]
 }
 
 #################
@@ -34,3 +34,40 @@ echo -e "${RED}$0${NOC}"
 printline
 echo -e "${NOC}this script installs Windows Emulation (Wine),\nRoller-Coaster-Tycoon 2,\nRoller-Coaster-Tycoon 2 Extension Pack${NOC}"
 printline
+
+container=rct
+
+rm -f $container/Dockerfile
+rmdir $container
+
+clear
+
+mkdir $container
+
+touch $container/Dockerfile
+tee $container/Dockerfile > /dev/null <<EOF
+FROM fedora
+
+RUN dnf update -y \
+	&& dnf install wine -y \
+	&& dnf clean all
+
+CMD /bin/bash
+EOF
+
+echo -e "${RED}created container $container and Dockerfile${NOC}"
+cat $container/Dockerfile
+
+cd $container
+echo -e "${RED}building container $container:${NOC} docker build -t $container ."
+sudo docker build -t $container .
+
+sleep 5
+
+echo -e "${RED}created docker image $container:${NOC} docker images"
+sudo docker images | grep $container
+
+sleep 5
+
+echo -e "${RED}running docker image $container:${NOC} docker run $container"
+sudo docker run -it $container
