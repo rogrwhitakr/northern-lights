3a284e973c65bfd444f18dd32b65bd734393c564
rogrwhitakr <bennoosterholt@gmail.com>
2017-06-25 20:22:20 +0200

    needs all read possibilities

---
+++ coreutils/read.sh
@@ -0,0 +1,25 @@
+#! bin/sh
+
+#################
+### functions ###
+#################
+
+printline() {
+	echo [---------------------------------------------------------------------]
+}
+
+#################
+### variables ###
+#################
+
+user=$(id -un)
+
+#################
+### execution ###
+#################
+
+printline
+#TODO
+#function for echo comment content
+#function gitsysnc
+#all read possibilities 
