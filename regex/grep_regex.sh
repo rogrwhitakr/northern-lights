100e83d6d2217a1969f8ddd4d38fdf44441728ed
rogrwhitakr <bennoosterholt@gmail.com>
2017-07-24 23:00:49 +0200

    regex / grep

--- regex/grep_regex.sh
+++ regex/grep_regex.sh
@@ -4,3 +4,6 @@ ip_address=192.168.100.100
 host=test
 
 grep -E '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' $ip_address
+
+# test this !!!
+ping -i 2 localhost | cut -d= -f4 | grep -E '0.[2-9]{3}'
