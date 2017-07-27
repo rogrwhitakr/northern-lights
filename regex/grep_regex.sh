e93f3bde4722f57ce3a5305d55ee6c3eaf531f0e
rogrwhitakr <bennoosterholt@gmail.com>
2017-07-27 07:47:56 +0200

    new todo list simply in the README. using the dashboard is not necessary

--- regex/grep_regex.sh
+++ regex/grep_regex.sh
@@ -6,4 +6,17 @@ host=test
 grep -E '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' $ip_address
 
 # test this !!!
-ping -i 2 localhost | cut -d= -f4 | grep -E '0.[2-9]{3}'
+ping -i 2 localhost | cut -d= -f4 | grep -E '[1-9]+.[0-9]{3}'
+
+# works
+grep -E '[1-9]+.[0-9]{3}' regex.template 
+
+# grep from stout
+
+grep -f -
+
+# -f is file
+#  - is stout 
+#  does not seem to work / proposed on stack oveflow
+  
+ping google.de | grep -E '[0-9]+\.[0-9]{1,3}\s'
