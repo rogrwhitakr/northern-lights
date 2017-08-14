67fbe296b3cfcfbf77770bc248001cd7f6dacfd2
rogrwhitakr <bennoosterholt@gmail.com>
2017-08-14 09:08:21 +0200

    made additions to awk

--- coreutils/awk.sh
+++ coreutils/awk.sh
@@ -1,22 +1,32 @@
 # /bin/sh
 
-RED='\033[0;31m'
-NOC='\033[0m' 	# No Color
+print_red() {
+	if [[ -z "$@" ]]; then
+		echo -e "print_red(): no args set"
+	fi	  	
+	local RED='\033[0;31m'
+	local NOC='\033[0m'
+	echo -e ${RED}"$@"${NOC}
+}
 
 function line() {
 	echo [------------------------------------------------------------------]
 }
 
-echo
-echo AWK
-echo
-echo -F == --field-separators
+######################## exec ###
 
-getent aliases | awk -F: '{ print $1,"||", $5 }'
+print_red AWK
+print_red principal functionality: 
 
+echo awk 'program' input-file1 input-file2 …
+echo awk -f program-file input-file1 input-file2 …
+echo awk [-F --field-separators] 'program' input-file1 input-file2 …
 line
 
-echo -e "${RED}Contents of /etc/passwd ${NOC}"
+getent aliases | awk -F: '{ print $1,"||", $5 }'
+line
+
+print_red Contents of /etc/passwd
 
 # print contents of /etc/passwd 
 cat /etc/passwd
@@ -27,11 +37,22 @@ awk -F: '{print $3,$4}' /etc/passwd
 line
 
 # print only the ones containing "systemd"
-awk -F: '$1 ~ /^systemd/ {print $5}' /etc/passwd
+awk -F: '$1 ~/^systemd/ {print $5}' /etc/passwd
 line
 
 # print using regex
-# awk -F  '$3 ~ /[0-9]{3}/ {print $1}' /etc/passwd
+awk -F  '$3 ~/^[0-9]{4,5}/$ {print $1,"|---|",$5}' /etc/passwd
+line
+
+# awk logic / can be added to a file and called as arg -f
+awk -F  '{
+	for(i=1;i<=NF;i++)
+		{ if($i=="/[0-9]{4,5}/")
+		{print $i} 
+		}
+	}' /etc/passwd
+
 # https://techarena51.com/blog/advance-text-processing-examples-awk/
 line
 
+exit 0
