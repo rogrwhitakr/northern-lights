059971ecb39cd33a56c5694c6a76250b04ccb790
rogrwhitakr <bennoosterholt@gmail.com>
2017-09-24 20:23:00 +0200

    made it into a cheatsheet

--- docker/how-to-docker.sh
+++ docker/how-to-docker.sh
@@ -1,28 +1,31 @@
 # bin/sh
 
-RED='\033[0;31m'
-NOC='\033[0m' 
+# install docker
+ 
+# manage group to allow users non-sudo docker control
+sudo groupadd docker
 
-#variabes
-container=docker.io/nginx:latest
-clear
-echo
-echo Docker Prozesse : Command [docker ps]
-echo
+# add user to docker group
+
+# get current locally available docker images
+docker images
+
+# get current docker processes
 docker ps
 
+# docker run options
+docker run
+	 -it [interactive]
+	 -d [detached]
+	 --rm 
+	 --help
+	 -p [port-forwarding 8080:8090 \| host:container] 
 
-echo
-echo vorhandenene docker images : Command [docker images]
-echo
-docker images
-read -n1 -r -p "Press key to continue..." key
+# default
+docker run -d <container>
 
-echo
-echo Docker run Options : -it [interactive] -d [detached] --rm --help -p [port-forwarding 8080:8090 \| host:container] 
-echo
+# docker stats
+docker stats
 
-#docker run --rm $container
-docker run -d $container
-read -n1 -r -p "Press key to continue..." key
-
+# docker logs
+docker logs
