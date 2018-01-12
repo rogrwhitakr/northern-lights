c8cbe50478a6dda99a5324afec25c77868e4f4f7
hankscorpio <bennoosterholt@gmail.com>
2018-01-12 15:16:50 +0100

    moved some stuff

--- apps/proxmox/install-proxmox.sh
+++ apps/proxmox/install-proxmox.sh
@@ -1,7 +1,12 @@
+
+# add repository
 echo "deb http://download.proxmox.com/debian jessie pve-no-subscription" > /etc/apt/sources.list.d/pve-install-repo.list
 
+# add key
 wget -O- "http://download.proxmox.com/debian/key.asc" | apt-key add -
 
+# refresh installation
 apt-get update && apt-get dist-upgrade
 
+# install PROXMOX
 apt-get install proxmox-ve ntp ssh postfix ksm-control-daemon open-iscsi systemd-sysv
