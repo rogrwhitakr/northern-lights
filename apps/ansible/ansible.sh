693727bab00e819868534e33b9fd865219b8f26b
rogrwhitakr <bennoosterholt@gmail.com>
2018-02-08 09:38:13 +0100

    added some commands from the documentation

--- apps/ansible/ansible.sh
+++ apps/ansible/ansible.sh
@@ -14,3 +14,9 @@ ansible northernlights -m ping
 
 # run command
 ansible northernlights -a "/bin/echo hello"
+
+# copy file to boxes
+ansible northernlights -m copy -a "src=~/./Bilder/wood-icon.png dest=/tmp/wood-icon.png"
+
+# check service state
+ansible northernlights -m service -a "name=httpd state=started"
