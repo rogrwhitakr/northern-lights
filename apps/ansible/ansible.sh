c5d68088bc1aa5745e497f95f27c815d948498db
rogrwhitakr <bennoosterholt@gmail.com>
2018-02-08 01:55:00 +0100

    ran demo command

--- apps/ansible/ansible.sh
+++ apps/ansible/ansible.sh
@@ -2,6 +2,15 @@
 # edit 
 # /etc/ansible/hosts
 # to declare hosts
+#
+# [northernlights]
+# server
+# dns
+# console
+# core
 
 # ping them
+ansible northernlights -m ping
 
+# run command
+ansible northernlights -a "/bin/echo hello"
