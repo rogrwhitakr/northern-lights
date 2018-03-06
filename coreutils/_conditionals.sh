c6cdf25ed544abe0fe03baa0dcdd3a1fa7389537
rogrwhitakr <bennoosterholt@gmail.com>
2018-03-06 07:31:10 +0100

    added file exists

--- coreutils/_conditionals.sh
+++ coreutils/_conditionals.sh
@@ -1,8 +1,15 @@
 #! /bin/dash
 programme="htop"
+demofile="~1.txt"
 
-if [ "which $programme" = "0" ]; then
+if ! [ which $programme = "0" ]; then
     echo "found $programme in $(which $programme)"
 else
     echo "nah"
+fi
+
+# checking if a file exists
+if [ ! -f $demofile ]; then
+	touch  $demofile
+	chmod 644 $demofile
 fi
