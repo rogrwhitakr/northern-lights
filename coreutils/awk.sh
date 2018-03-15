9332645b414197d1c8954b90d12c2cd9fdbbe665
rogrwhitakr <bennoosterholt@gmail.com>
2018-03-15 18:04:27 +0100

    add some sample to quickly choose from

--- coreutils/awk.sh
+++ coreutils/awk.sh
@@ -1,48 +1,29 @@
 # /bin/sh
+# AWK
 
-print_red() {
-	if [[ -z "$@" ]]; then
-		echo -e "print_red(): no args set"
-	fi	  	
-	local RED='\033[0;31m'
-	local NOC='\033[0m'
-	echo -e ${RED}"$@"${NOC}
-}
+# principal functionality
+awk 'program' input-file1 input-file2 …
+awk [-F --field-separators] 'program' input-file1 input-file2 …
 
-function line() {
-	echo [------------------------------------------------------------------]
-}
-
-######################## exec ###
-
-print_red AWK
-print_red principal functionality: 
-
-echo awk 'program' input-file1 input-file2 …
-echo awk -f program-file input-file1 input-file2 …
-echo awk [-F --field-separators] 'program' input-file1 input-file2 …
-line
-
+# get aliases and print 
 getent aliases | awk -F: '{ print $1,"||", $5 }'
-line
-
-print_red Contents of /etc/passwd
-
-# print contents of /etc/passwd 
-cat /etc/passwd
-line
 
 # print only the GID and UID columns
 awk -F: '{print $3,$4}' /etc/passwd
-line
+
+# print last, third, third-to-last field
+# use quotes to avoid clashes with shell special characters
+echo 'one;2018-02-12;two;CLASH;four;five;nej;2;464;ERROR' | awk -F';' '{print $(NF), "|", $2, "|", $(NF-3)}'
+
+# Regular expressions based input field separator
+
+
 
 # print only the ones containing "systemd"
 awk -F: '$1 ~/^systemd/ {print $5}' /etc/passwd
-line
 
 # print using regex
 awk -F  '$3 ~/^[0-9]{4,5}/$ {print $1,"|---|",$5}' /etc/passwd
-line
 
 # awk logic / can be added to a file and called as arg -f
 awk -F  '{
@@ -54,7 +35,3 @@ awk -F  '{
 
 # https://techarena51.com/blog/advance-text-processing-examples-awk/
 # https://github.com/learnbyexample/Command-line-text-processing/blob/master/gnu_awk.md
-
-line
-
-exit 0
