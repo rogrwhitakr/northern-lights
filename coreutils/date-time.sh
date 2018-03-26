a881d17c3eed795f5f1cc252c45dea5aad4b52d9
rogrwhitakr <bennoosterholt@gmail.com>
2018-03-26 06:53:16 +0200

    moved this

--- coreutils/date-time.sh
+++ coreutils/date-time.sh
@@ -38,3 +38,10 @@ echo -e "$DATE\n$TIME"
 # used by history command 
 
 export HISTTIMEFORMAT='%Y-%m-%d_%H:%M'
+
+MONTH=25
+DATE=`date +%Y-%m-%d`
+DISABLE_DATE=`date -d '-6 months ago' +%Y-%m-%d`
+_DATE=$(sed -e 's/6 months ago/${MONTH} months ago/g' <<< $DISABLE_DATE)
+
+#_DATE='sed -i "s/6/7/g" $DISABLE_DATE'
