c1dc9351a65971cff1f201e3635d7787ea611ca5
rogrwhitakr <bennoosterholt@gmail.com>
2018-03-27 08:07:43 +0200

    moar

--- coreutils/sed.sh
+++ coreutils/sed.sh
@@ -31,3 +31,21 @@ has_backslash $@
 SQL="select claim_id from claim;"
 SQL=$(sed -e "s/claim/setting/g" <<< $SQL)
 echo -e $SQL
+
+# found this on reddit, does some polling of stats?
+# anyway it has some sed in it
+# maybe take it apart and see what it does
+# looks for a digit like 0.0000123 or 312.38487
+# seems to be this posix thingy
+
+for i in {1..100}; do
+    strace -T -e statfs df . 2>&1
+done |
+    sed -n '
+      # output looks like: statfs(".", {f_foo=BAR, …}) = 0 <0.000241>
+      /^.* = <\([[:digit:]]\+\.[[:digit:]]\+\)>$/ {
+        s//\1/
+        p
+      }
+    ' |
+    sort -n
