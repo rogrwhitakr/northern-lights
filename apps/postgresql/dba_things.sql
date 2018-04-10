234e4b6ea3b0a3013f33cfd35b9effcf05633159
rogrwhitakr <bennoosterholt@gmail.com>
2018-04-10 07:29:55 +0200

    aDDED dump varitions according to dicumentation

--- apps/postgresql/dba_things.sql
+++ apps/postgresql/dba_things.sql
@@ -29,3 +29,18 @@ pg_dump -h <host> -p <port>
 
 --Restoring the Dump
 psql dbname < infile
+
+--database name will not be created by this command, so
+--it must created from template0 before executing psql
+createdb -T template0 dbname
+--Non-text file dumps are restored using the pg_restore utility.
+
+-- stop on error:
+psql --set ON_ERROR_STOP=on dbname < infile
+-- specify single transaction
+psql --single-transaction 
+-- is pipable:
+pg_dump -h host1 dbname | psql -h host2 dbname
+-- Important: The dumps produced by pg_dump are relative to template0. 
+-- This means that any languages, procedures, etc. added via template1 will also be dumped by pg_dump. 
+-- As a result, when restoring, if you are using a customized template1, you must create the empty database from template0.
