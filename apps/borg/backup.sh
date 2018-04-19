489ee451ddfe4dad307ae83db7457f6fd3294ea1
rogrwhitakr <bennoosterholt@gmail.com>
2018-04-19 07:05:08 +0200

    deutsch -> english , some changes

--- apps/borg/backup.sh
+++ apps/borg/backup.sh
@@ -1,63 +1,64 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # Skriptvorlage BorgBackup
 # https://wiki.ubuntuusers.de/BorgBackup/
 # https://borgbackup.readthedocs.io/en/stable/
+# should:
+# -> create user specific for this task?
+# -> if yes, what are the benefits?
+# -> 
 
+# path to backup target
+backup_target="/mnt/backup"
 
-# Hier Pfad zum Sicherungsmedium angeben.
-# z.B. zielpfad="/media/peter/HD_Backup"
-zielpfad="/home/alonsoquinn"
+# name of backup repository / one level down
+repository="borg"
 
-# Hier Namen des Repositorys angeben.
-# z.B. repository="borgbackups"
-repository="borgbackup"
+# list of 
+# ->  files / dirs to backup
+# ->  files / dirs to exclude from within these lists
+# includes="/home/peter/Bilder /home/peter/Videos --exclude *.tmp"
+includes="/home/alonsoquinn --exclude '/home/alonsoquinn/.*'"
 
-# Hier eine Liste mit den zu sichernden Verzeichnissen angeben
-# z.B. sicherung="/home/peter/Bilder /home/peter/Videos --exclude *.tmp"
-sicherung="/home/alonsoquinn --exclude '/home/alonsoquinn/.*'"
-# sicherung="/home/alonsoquinn"
-# Hier die Art der Verschlüsselung angeben
-# z.B. verschluesselung="none"
-verschluesselung="none"
+# mode of encryption / options = "none, ???"
+encryption="none"
 
-# Hier die Art der Kompression angeben
-# z.B. kompression="none"
-kompression="lz4"
+# mode of compression / options = "none, lz4, ???"
+compression="lz4"
 
 # Hier angeben, ob vor der Ausführung von BorgBackup auf vorhandene Root-Rechte geprüft werden soll
-# z.B. rootuser="ja"
-rootuser="nein"
+# _root="ja"
+_root="nein"
 
 # Hier angeben nach welchem Schema alte Archive gelöscht werden sollen.
-# Die Vorgabe behält alle Sicherungen des aktuellen Tages. Zusätzlich das aktuellste Archiv der 
-# letzten 7 Sicherungstage, der letzten 4 Wochen sowie der letzten 12 Monate.
-pruning="--keep-within=1d --keep-daily=7 --keep-weekly=4 --keep-monthly=12"
+# Die Vorgabe behält alle sicherungen des aktuellen Tages. Zusätzlich das aktuellste Archiv der 
+# letzten 7 includesstage, der letzten 4 Wochen sowie der letzten 12 Monate.
+prune="--keep-within=1d --keep-daily=7 --keep-weekly=4 --keep-monthly=12"
 
 ###################################################################################################
 
-repopfad="$zielpfad"/"$repository"
+repopfad="$backup_target"/"$repository"
 
 # check for root
-if [ $(id -u) -ne 0 ] && [ "$rootuser" == "ja" ]; then
-  echo "Sicherung muss als Root-User ausgeführt werden."
+if [ $(id -u) -ne 0 ] && [ "$_root" == "ja" ]; then
+  echo "includes muss als Root-User ausgeführt werden."
   exit 1
 fi
 
 # Init borg-repo if absent
 if [ ! -d $repopfad ]; then
-  borg init --encryption=$verschluesselung $repopfad 
+  borg init --encryption=$encryption $repopfad 
   echo "Borg-Repository erzeugt unter $repopfad"
 fi
 
 # backup data
 SECONDS=0
-echo "Start der Sicherung $(date)."
+echo "Start der includes $(date)."
 
-borg create --compression $kompression --exclude-caches --one-file-system -v --stats --progress \
-            $repopfad::'{hostname}-{now:%Y-%m-%d-%H%M%S}' $sicherung
+borg create --compression $compression --exclude-caches --one-file-system -v --stats --progress \
+            $repopfad::'{hostname}-{now:%Y-%m-%d-%H%M%S}' $includes
 
-echo "Ende der Sicherung $(date). Dauer: $SECONDS Sekunden"
+echo "Ende der includes $(date). Dauer: $SECONDS Sekunden"
 
 # prune archives
-borg prune -v --list $repopfad --prefix '{hostname}-' $pruning
+borg prune -v --list $repopfad --prefix '{hostname}-' $prune
