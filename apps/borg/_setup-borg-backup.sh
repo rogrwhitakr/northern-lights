b5b073d12c9a87e07f8165dec705326fc11e3223
rogrwhitakr <bennoosterholt@gmail.com>
2018-05-23 07:05:02 +0200

    faster changes

---
+++ apps/borg/_setup-borg-backup.sh
@@ -0,0 +1,24 @@
+#! /usr/bin/env sh
+
+echo "this script is used to
+->  set up BORG backup script 
+->  set up systemd unit files (service and timer)
+using files found in this directory"
+
+ls *.sh
+ls *.timer
+ls *.service
+
+read -rp $'Continue (Y/n) : ' -ei $'Y' continue_key;
+echo "$continue_key"
+
+if [[ "$continue_key" != "Y" ]]; then
+    echo "user-terminated. exiting..."
+    exit 0
+fi
+
+# moving unit files to correct directory
+
+sudo cp borg-backup.timer /etc/systemd/system/borg-backup.timer
+sudo cp borg-backup.service /etc/systemd/system/borg-backup.service
+exit 0
