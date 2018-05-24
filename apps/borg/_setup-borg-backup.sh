06ac46b35c22b87159f989bfb59a79225ba1cb3a
rogrwhitakr <bennoosterholt@gmail.com>
2018-05-24 17:01:18 +0200

    added a somewhat complete function to mitigate ....things

--- apps/borg/_setup-borg-backup.sh
+++ apps/borg/_setup-borg-backup.sh
@@ -1,16 +1,25 @@
 #! /usr/bin/env sh
 
-echo "this script is used to
+echo "
+this script ($0) is used to
 ->  set up BORG backup script 
 ->  set up systemd unit files (service and timer)
-using files found in this directory"
+    using files found in this directory
+"
+
+# if the files cannot be found i am not in the right directory
+if [[ $(ls borg-backup.timer 2> /dev/null) != 0 ]] || \
+    [[ $(ls borg-backup.service 2> /dev/null) != 0 ]]; then
+    echo "systemd unit files not found in current directory"
+    readonly dir=$(readlink "$0")
+    echo "current location:"
+    pwd
+    echo "Exiting $0, process-id $$"
+    exit 1
+fi    
 
 filename=$(basename "$0")
 echo "$filename"
-
-ls *.sh
-ls *.timer
-ls *.service
 
 read -rp $'Continue (Y/n) : ' -ei $'Y' continue_key;
 echo "$continue_key"
