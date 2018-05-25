2e197b9e4d60970e69408de419c28c0812dca9b0
rogrwhitakr <bennoosterholt@gmail.com>
2018-05-25 16:50:22 +0200

    need to be distributed to other files really

--- coreutils/script/_from-bash-explainer.sh
+++ coreutils/script/_from-bash-explainer.sh
@@ -8,8 +8,11 @@ for file in $(ls); do
     echo ${file%.*} 
 done
 
-# Use $(...) instead of `which <programme>
-`
+# use built-ins as much as possible
+printf '%d\t' {1..10}
+printf '\n'
+# Use $(...) instead of `which <programme>`
+
 # Use built-in math instead of expr to do simple calculations, especially when just incrementing a variable.
 # If the script you're reading uses x=`expr $x + 1` then it's not something you want to mimic.
 
@@ -23,3 +26,18 @@ for friend in "${friends[@]}";do
     echo "$friend is my friend";
 done
 
+# ALWAYS use double [[ ]] brackets, they are saver
+# -> [ is the test command
+
+simple_var="rubarb"
+less_simple_var="123Gak48$"
+
+[[ "$simple_var" = "rubarb" ]] && echo True || echo False
+
+# pattern matching (NO QUOTES THIS TIME!!)
+[[ $simple_var = ru* ]] && echo True || echo False
+[[ $less_simple_var =~ \d\d\d.+ ]] && echo True || echo False
+
+# belongs to loops
+i=8
+let i++ 
