2b35f4d24ddeca821e99840e91b0046719d63998
HS <HS@northernlights.io>
2018-06-07 10:05:01 +0200

    getopts more stuff

--- coreutils/script/getopts.bash
+++ coreutils/script/getopts.bash
@@ -2,6 +2,8 @@
 
 # example from stack overflow,
 # needs some explainers
+# http://wiki.bash-hackers.org/howto/getopts_tutorial
+# https://sookocheff.com/post/bash/parsing-bash-script-arguments-with-shopts/
 
 usage() { echo "Usage: $0 [-s <45|90>] [-p <string>]" 1>&2; exit 1; }
 
@@ -10,23 +12,31 @@ while getopts ":s:p:mf:" o; do
         s)
             s=${OPTARG}
             ((s == 45 || s == 90)) || usage
+            echo "s = ${s}"
             ;;
         p)
             p=${OPTARG}
+            echo "p = ${p}"
             ;;
         m)
             m=${OPTARG}
+            echo "m = ${m}"
             ;;
+        \?)
+            echo "Invalid option: -$OPTARG" >&2
+            ;;    
         *)
-            usage
+            echo "in *)"
             ;;
     esac
 done
 shift $((OPTIND-1))
 
-if [ -z "${s}" ] || [ -z "${p}" ] || [ -z "${mf" ]; then
+check(){
+if [ -z "${s}" ] || [ -z "${p}" ] || [ -z "${m}" ]; then
     usage
 fi
+}
 
 echo "s = ${s}"
 echo "p = ${p}"
