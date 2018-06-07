fcffd0c84e837cc783dd164efbcc0cd40e7b3baf
rogrwhitakr <bennoosterholt@gmail.com>
2018-06-07 17:11:42 +0200

    added to array explainer

--- coreutils/script/_array.bash
+++ coreutils/script/_array.bash
@@ -1,7 +1,18 @@
-
+#! /usr/bin/env bash
 
 # Arrays are friends....
 friends=( "Marcus The Rich" "JJ The Short" "Timid Thomas" "Michelangelo The Mobster" )
 for friend in "${friends[@]}";do
     echo "$friend is my friend";
+done
+
+# also possible:
+# get length of an array
+echo ${#friends[@]}
+length=${#friends[@]}
+ 
+# use for loop to read all names
+for (( i=0; i<${length}; i++ ));
+do
+  echo ${friends[$i]}
 done
