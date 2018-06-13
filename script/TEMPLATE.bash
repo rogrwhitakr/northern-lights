811daf29438ac4181b1592c39b38c183163b0c92
rogrwhitakr <bennoosterholt@gmail.com>
2018-06-13 07:31:25 +0200

    #!

---
+++ script/TEMPLATE.bash
@@ -0,0 +1,121 @@
+#! /usr/bin/env bash
+
+# ######################################################################################
+# BASH SCRIPT TEMPLATE
+#   HISTORY:
+#
+#   2018-06-13 - v1.0.0  - First Creation (mostly by stealing from others)
+#
+# ######################################################################################
+# VARIABLES
+#   VERSION
+version="1.0.0"
+
+#   FLAGS - can be overridden by user input?
+quiet=0
+printLog=0
+verbose=0
+force=0
+strict=0
+debug=0
+args=()
+
+# ######################################################################################
+#   functions -> these need to go somehere else sometime, as they are helpers...
+
+usage(){
+
+    local RED='\e[1;31m'  
+    local NOC='\033[0m'
+
+    echo -e "${RED}${scriptName} [OPTION]... [FILE]...${NOC}
+
+script template
+
+${RED} Options:${NOC}
+    -u  Username for script
+    -p  User password
+    -f  force, skip all user interaction.  Implied 'Yes' to all actions.
+    -q  Quiet (no output)
+    -l  Print log to file
+    -s  Exit script with null variables.  i.e 'set -o nounset'
+    -v  verbose
+    -d  run script in debug-mode (set -x)
+    -h  Display this help and exit
+    -v  Output version information and exit
+
+${RED} Prerequisites:${NOC}
+    - file is located within home directory
+    - file is one of *.service, (*.timer)
+"
+}
+usage
+
+# UNSURE about this:
+# Trap bad exits with your cleanup function
+trapCleanup(){
+  echo "TODO"
+}
+trap trapCleanup EXIT INT TERM
+
+# Exit on error. Append '||true' when you run the script if you expect an error.
+set -o errexit
+
+# Run in debug mode, if set
+if [ "${debug}" == "1" ]; then
+  set -x
+fi
+
+# Exit on empty variable
+if [ "${strict}" == "1" ]; then
+  set -o nounset
+fi
+
+# Bash will remember & return the highest exitcode in a chain of pipes.
+# This way you can catch the error in case mysqldump fails in `mysqldump |gzip`, for example.
+set -o pipefail
+
+# DESC: Generic script initialisation
+# ARGS: $@ (optional): Arguments provided to the script
+# OUTS: $orig_cwd: The current working directory when the script was run
+#       $script_path: The full path to the script
+#       $script_dir: The directory path of the script
+#       $script_name: The file name of the script
+#       $script_params: The original parameters provided to the script
+#       $ta_none: The ANSI control code to reset all text attributes
+# NOTE: $script_path only contains the path that was used to call the script
+#       and will not resolve any symlinks which may be present in the path.
+#       You can use a tool like realpath to obtain the "true" path. The same
+#       caveat applies to both the $script_dir and $script_name variables.
+function script_init() {
+    # Useful paths
+    readonly orig_cwd="$PWD"
+    readonly script_path="${BASH_SOURCE[1]}"
+    readonly script_dir="$(dirname "$script_path")"
+    readonly script_name="$(basename "$script_path")"
+    readonly script_params="$*"
+
+    # Important to always set as we use it in the exit handler
+    readonly ta_none="$(tput sgr0 2> /dev/null || true)"
+}
+
+# stolen from github
+# DESC: Main control flow
+# ARGS: $@ (optional): Arguments provided to the script
+# OUTS: None
+function main() {
+    # shellcheck source=source.sh
+    source "$(dirname "${BASH_SOURCE[0]}")/source.sh"
+
+    trap script_trap_err ERR
+    trap script_trap_exit EXIT
+
+    script_init "$@"
+    parse_params "$@"
+    cron_init
+    colour_init
+}
+
+
+# Make it rain
+main "$@"
