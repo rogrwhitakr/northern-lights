b5231de13ae40e4059a2da0744d5aa8d129cc77d
rogrwhitakr <bennoosterholt@gmail.com>
2018-06-18 16:45:43 +0200

    updates

--- README.md
+++ README.md
@@ -21,29 +21,29 @@
 ## things to do:
 
 ### CHECK:
-screen 
-chubby / Zookeeper lock service
+- screen 
+- chubby / Zookeeper lock service
 
 ### WHAT?
-what is webrick exactly?
-what is "rake" exactly?
+- what is webrick exactly?
+- what is "rake" exactly?
+- what is inetd
 
 ### WHY?
-copying a file from home to root-owned directory /var/www/html makes root the owner? Even if apache user owns the directory and my user is part of the apache group?
+- copying a file from home to root-owned directory /var/www/html makes root the owner? Even if apache user - owns the directory and my user is part of the apache group?
 
 ### COREUTILS
-how do i get the owner of the file in uid and guid ids?
-find -perm setuid-bit, like on /bin/login program
+- how do i get the owner of the file in uid and guid ids?
+- find -perm setuid-bit, like on /bin/login program
 
 ### APACHE
-How do you measure webserver performance / load? 
--> If i use a memcached service layer, how do i initially decide i need one?
-->mal suchen nach mime.types (Apache)
-->inetd - was ist das?
-->Load balancing
-->Common Gateway Interface
-->CGI programs serve HTML data dynamically based on input passed to them
+- How do you measure webserver performance / load? 
+  - If i use a memcached service layer, how do i initially decide i need one?
+  - explainer for mime.types
+- Load balancing
+- Common Gateway Interface
+- CGI programs serve HTML data dynamically based on input passed to them
 
 ### BORG:
-how to put backup back into play?
-TODO: extraction script, using the latest backup (or one that can be chosen?)
+- how to put backup back into play?
+- TODO: extraction script, using the latest backup (or one that can be chosen?)
--- coreutils/date-time.sh
+++ coreutils/date-time.sh
@@ -12,7 +12,8 @@ COMMAND="date --utc"
 echo -e "${RED}$COMMAND${NOC}"
 $COMMAND
 
-args="%H		The hour in the HH format (for example, 17).
+args="
+	%H		The hour in the HH format (for example, 17).
 	%M		The minute in the MM format (for example, 30).
 	%S		The second in the SS format (for example, 24).
 	%d		The day of the month in the DD format (for example, 16).
@@ -24,7 +25,7 @@ args="%H		The hour in the HH format (for example, 17).
 
 COMMAND=date +"%Y-%m-%d"
 
-echo -e '\n' "${RED}"date + format '\n'${NOC} "Format = $args"'\n'
+echo -e '\n' "${RED}"date + format '\n'${NOC} "Formatting = $args"'\n'
 
 echo -e "command = $COMMAND"
 $COMMAND
@@ -55,3 +56,22 @@ echo $DIFF
 # i assume an intermediate step is necessary
 # date -d '-6 months ago' +%Y-%m-%d
 echo $(( ($(date --date="2017-05-21 21:18:30" +%s) - $(date --date="2017-05-21 20:28:57" +%s) )/(60*60*24) ))
+
+# example dates taken from journal --list-boots
+# Fri 2017-06-09 22:42:01 CEST — Sat 2017-06-10 00:13:14 CEST
+# Sat 2017-06-10 08:54:25 CEST — Sun 2017-06-11 19:48:35 CEST
+# Thu 2017-06-15 11:07:42 CEST — Mon 2017-06-19 06:23:24 CEST
+# Thu 2017-06-22 20:21:43 CEST — Fri 2017-06-23 07:45:54 CEST
+# Sat 2017-06-24 00:07:19 CEST — Sun 2017-06-25 20:22:57 CEST
+# Fri 2017-06-30 18:55:58 CEST — Mon 2017-07-10 03:48:47 CEST
+# Mon 2017-07-10 06:38:56 CEST — Mon 2017-07-10 21:36:43 CEST
+# Mon 2017-07-10 21:37:37 CEST — Tue 2017-07-18 20:35:56 CEST
+
+# subtracting or adding elements
+startup_datetime="2017-06-10 08:54:25"
+shutdown_datetime="2017-06-11 19:48:35"
+# subtracting or adding elements
+startup_date="2017-06-10"
+shutdown_date="2017-06-11"
+date --date="${shutdown_date} -${startup_date} day" +%Y-%m-%d
+#date --date="${shutdown_datetime} - ${startup_datetime} day" +%Y-%m-%d %h:%M:%s
