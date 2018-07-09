188a76d26c79c9ace412a2053d7cd9b6eae7f6d9
rogrwhitakr <bennoosterholt@gmail.com>
2018-07-09 06:50:36 +0200

    iw, networkung hardware

---
+++ network/NIC-notes.bash
@@ -0,0 +1,16 @@
+#! /usr/bin/env bash
+
+# iw 
+#   iw - show / manipulate wireless devices and their configuration
+#   this seems to be a powerful tool
+
+# list devices
+iw list
+# -> this does not show the interface name, like enp0s3, wlp4s0 or eth1 
+
+# get the ssid of current network
+iw dev wlp4s0 info | grep ssid
+
+# go to 
+/sys/class/net
+grep "" enp3s0/*
