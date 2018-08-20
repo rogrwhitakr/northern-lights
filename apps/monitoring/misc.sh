1955c4f5ecdf1e3c7e0e4e7d454476d3937c6f5b
benno <benno@HankScorpio.corp.optitool.de>
2018-08-20 15:11:00 +0200

    updated the workstation setup, lets see if it works

---
+++ apps/monitoring/misc.sh
@@ -0,0 +1,22 @@
+# see
+https://kkovacs.eu/cool-but-obscure-unix-tools
+
+dstat 
+sar
+dstat 
+screenshot
+slurm
+screen
+dtach
+powertop
+qalc
+htop
+atop
+glances
+ttyrec
+mtr
+socat
+netpipes
+iftop
+iptraf
+nethogs
