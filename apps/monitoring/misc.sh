0710aaad09aa09fe8cc1690c457221158d4d71b1
benno <benno@HankScorpio.corp.optitool.de>
2018-08-20 15:56:03 +0200

    removed unavailable packages

--- apps/monitoring/misc.sh
+++ apps/monitoring/misc.sh
@@ -1,22 +1,15 @@
 # see
-https://kkovacs.eu/cool-but-obscure-unix-tools
+# https://kkovacs.eu/cool-but-obscure-unix-tools
 
-dstat 
-sar
-dstat 
-screenshot
-slurm
-screen
-dtach
-powertop
-qalc
-htop
-atop
-glances
-ttyrec
-mtr
-socat
-netpipes
-iftop
-iptraf
-nethogs
+sudo dnf repoquery -y dstat 
+sudo dnf repoquery -y screenshot
+sudo dnf repoquery -y slurm
+sudo dnf repoquery -y screen
+sudo dnf repoquery -y dtach
+sudo dnf repoquery -y powertop
+sudo dnf repoquery -y htop
+sudo dnf repoquery -y atop
+sudo dnf repoquery -y glances
+sudo dnf repoquery -y mtr
+sudo dnf repoquery -y iftop
+sudo dnf repoquery -y nethogs
