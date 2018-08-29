82c9d3382fe81c528c8cc1045ff841cb63fe3175
rogrwhitakr <bennoosterholt@gmail.com>
2018-08-29 07:23:53 +0200

    some addses, can handle different extensions now

--- coreutils/regex.bash
+++ coreutils/regex.bash
@@ -25,18 +25,38 @@ date="20180s12"
 #   remove trailing whatevers
 
 cd ~/Downloads
+cd /mnt/backup/video
 #file='Ansible - an absolute basic overview-MfoAb50Br94.mp4'
 pwd
 # get the files
 
-files=()
-for file in *.webm; do
+# do something depedning on extension (or any other part of the name, really...) 
+for file in *; do
+	case "${file#*.}" in
+	mp4)
+		variable=gay
+		print GREEN "${variable}"
+		;;
+	webm)
+		b="relative"
+		print YELLOW "${b}"
+		;;
+	*)
+		print RED "${file}"
+		print RED "Unexpected expression '${file#*.}'"
+		;;
+	esac
+done
+
+for file in *; do
 	print GREEN "processing file ${file}"
 	src="${file}"
+	ext="${file#*.}"
 	print YELLOW "removing the trailing part, that youtube-dl creates."
-	file="${file%-*.mp4}.mp4"
+	file="${file%-*.${ext}}.${ext}"
 	print YELLOW "removing dashes,commas,etc"
-	file="${file//-/}"
+# this is a problem when reexecuting!!
+#	file="${file//-/}"
 	file="${file//,/}"
 	file="${file//(/}"
 	file="${file//)/}"
