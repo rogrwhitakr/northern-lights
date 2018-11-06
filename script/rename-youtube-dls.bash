5e3e22490ea5881fb0a91b8a97f19639892d5cbb
rogrwhitakr <bennoosterholt@gmail.com>
2018-11-06 07:08:33 +0100

    removed the systemd-cat, as all that is printed will be logged anyways..

--- script/rename-youtube-dls.bash
+++ script/rename-youtube-dls.bash
@@ -20,7 +20,6 @@ debug=0
 
 script_init
 flags_init
-log_init
 
 # set directory videos sit in
 video_dir="/mnt/backup/video"
@@ -34,20 +33,20 @@ regexp_rename_spec() {
 		local src="${file}"
 		local ext="${file##*.}"
 
-		print "BEFORE: ${file}" | systemd-cat
+		print "BEFORE: ${file}"
 		# remove extension
 		file="${file//.${ext}/}"
-		print "remove extension: ${file}" | systemd-cat
+		print "remove extension: ${file}"
 		# remove youtube specifier
 		if [[ "${qualifier}" == true ]]; then
 			file="${file%%-*}"
 		elif [[ "${qualifier}" == false ]]; then
 			file="${file%-*}"
 		fi
-		print "removing youtube specifier: ${file}" | systemd-cat
+		print "removing youtube specifier: ${file}"
 		# removing everything that is NOT [A-Za-z0-9]"
 		file="${file//[^A-Za-z0-9]/_}"
-		print "removing everything that is NOT [A-Za-z0-9]: ${file}" | systemd-cat
+		print "removing everything that is NOT [A-Za-z0-9]: ${file}"
 		
 		# removing doubles and triples and so forth
 		file="${file//______/_}"
@@ -55,20 +54,20 @@ regexp_rename_spec() {
 		file="${file//____/_}"
 		file="${file//___/_}"
 		file="${file//__/_}"
-		print "removing doubles and triples and so forth: ${file}" | systemd-cat
+		print "removing doubles and triples and so forth: ${file}"
 
 		# removing any leftover underscores from end of string
 		file="${file%_*}"
-		print "removing any leftover underscores from end of string: ${file}" | systemd-cat
+		print "removing any leftover underscores from end of string: ${file}"
 
 		# reappending extension
 		file="${file}.${ext}"
 		if [[ "${src}" != "${file}" ]]; then
 			mv "${src}" "${file}"
 		fi
-		print "AFTER: ${file}" | systemd-cat
+		print "AFTER: ${file}"
 	else
-		print "no regular file passed. doing nothing" | systemd-cat
+		print "no regular file passed. doing nothing"
 	fi
 }
 
@@ -80,17 +79,23 @@ main() {
 		ext="${file##*.}"                                      # extension
 		ye="${file:$((${#file} - ${yt_chars} - 2 - ${#ext}))}" # youtube+extension
 
-		print YELLOW "aggressive count: file: ${#file} :: file%%-*: ${#ac} :: ytext: ${#ye}" | systemd-cat
-		print YELLOW "non-aggressive count: file: ${#file} :: file%-*: ${#nac} :: ytext: ${#ye}" | systemd-cat
+		print LINE
+		print YELLOW "aggressive count: file: ${#file} :: file%%-*: ${#ac} :: ytext: ${#ye}"
+		print YELLOW "non-aggressive count: file: ${#file} :: file%-*: ${#nac} :: ytext: ${#ye}"
 
 		if [[ "$((${#file} - ${#ac} - ${#ye}))" == 0 ]]; then
 			regexp_rename_spec "${file}" true
 		elif [[ "$((${#file} - ${#nac} - ${#ye}))" == 0 ]]; then
 			regexp_rename_spec "${file}" false
 		else
-			print "not renaming file "${file}". Continuing" | systemd-cat
+			print "not renaming file "${file}". Continuing"
 		fi
 	done
 }
+
+print LINE
+print GREEN "Start of script ${0}"
+print LINE
+print ""
 
 main "${@}"
