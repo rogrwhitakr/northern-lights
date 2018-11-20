29b61092aaa10b48ab8e6bcbc226f272b96cc2e3
HankScorpio <benno@HankScorpio.mshome.net>
2018-11-20 14:59:54 +0100

    addses and changes

---
+++ coreutils/install.bash
@@ -0,0 +1,18 @@
+#!/usr/bin/env sh
+
+unit_service_file=/home/benno/northern-lights/systemd/profile-setup.service
+user_unit_file_dir=/etc/systemd/system
+
+echo 'start'
+# works!
+# sudo systemd-analyze verify "${unit_service_file}"
+
+# okay, lets see if we can use this for service file "installation"
+install "${unit_service_file}" \
+    --target-directory "${user_unit_file_dir}" \
+    --compare \
+    --mode=622
+
+ls -lah "${user_unit_file_dir}/${unit_service_file##*/}"
+stat "${user_unit_file_dir}/${unit_service_file##*/}"
+# enabling comes later, trapping any things ealierer   
