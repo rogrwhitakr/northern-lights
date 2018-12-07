b691ce772009ce666619438d31932ad7d24d6a51
rogrwhitakr <bennoosterholt@gmail.com>
2018-12-07 07:12:03 +0100

    added example

--- coreutils/install.bash
+++ coreutils/install.bash
@@ -1,5 +1,21 @@
 #!/usr/bin/env sh
 
+# a very convenient one-liner to set all properties of a file / directory:
+
+sudo install create-database.sql \ 
+    --target-directory /var/lib/pgsql/scripts \
+    --owner=postgres \
+    --group=postgres \
+    --compare \
+    --mode=640
+
+# this way, the regular user may edit the file in an environment, that is well known
+# the file is provided in an easily executable way for the postgres user in a known, convenient directory      
+
+
+# [--------------------------------------------------------------]
+
+
 unit_service_file=/home/benno/northern-lights/systemd/profile-setup.service
 user_unit_file_dir=/etc/systemd/system
 
@@ -16,3 +32,15 @@ install "${unit_service_file}" \
 ls -lah "${user_unit_file_dir}/${unit_service_file##*/}"
 stat "${user_unit_file_dir}/${unit_service_file##*/}"
 # enabling comes later, trapping any things ealierer   
+
+# a very convenient one-liner to set all properties of a file / directory:
+
+sudo install create-database.sql \ 
+    --target-directory /var/lib/pgsql/scripts \
+    --owner=postgres \
+    --group=postgres \
+    --compare \
+    --mode=640
+
+# this way, the regular user may edit the file in an environment, that is well known
+# the file is provided in an easily executable way for the postgres user in a known, convenient directory      
