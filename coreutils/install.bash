705e55f63c1373ded0821607948780e6ef429c2d
rogrwhitakr <bennoosterholt@gmail.com>
2018-12-07 07:24:41 +0100

    okay

--- coreutils/install.bash
+++ coreutils/install.bash
@@ -9,6 +9,13 @@ sudo install create-database.sql \
     --compare \
     --mode=640
 
+sudo install create-database.sql \ 
+    --no-target-directory /var/lib/pgsql/scripts/sql-file.sql \ # treat as normal file
+    --owner=postgres \
+    --group=postgres \
+    --compare \
+    --mode=640
+
 # this way, the regular user may edit the file in an environment, that is well known
 # the file is provided in an easily executable way for the postgres user in a known, convenient directory      
 
