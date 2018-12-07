781b2bff152c4c1f123f0b552459fd2027ed26c7
rogrwhitakr <bennoosterholt@gmail.com>
2018-12-07 07:23:43 +0100

    install does not like separate lines it seems

--- conf/network/config/install-hosts-file.bash
+++ conf/network/config/install-hosts-file.bash
@@ -1,20 +1,14 @@
 #!/usr/bin/env bash
 
 # VERSION
-version="1.0.1"
+version="0.0.1"
 
 source "/home/admin/MyScripts/script/helpers/init.bash"
-source "/home/admin/MyScripts/script/helpers/log.bash"
 
 strict=1
-debug=0
+debug=1
 
 script_init
 flags_init
 
-sudo install create-database.sql \ 
-    --target-directory /var/lib/pgsql/scripts \
-    --owner=postgres \
-    --group=postgres \
-    --compare \
-    --mode=640
+sudo install --no-target-directory hosts.northernlights.io /etc/hosts --mode=644
