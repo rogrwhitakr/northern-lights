6f0eb0a23037489483d039bec08dfac2b009e887
rogrwhitakr <bennoosterholt@gmail.com>
2018-12-12 06:58:30 +0100

    added sourcing of helpers, removed duplicates

--- script/setup-profile-on-new-box.bash
+++ script/setup-profile-on-new-box.bash
@@ -8,88 +8,23 @@
 #   2018-06-23 - v0.1.1  -  add one-shot service for pulling files
 #
 # ######################################################################################
-# VARIABLES
-#   VERSION
-version="0.1.1"
 
-#   FLAGS
+# SOURCES
+# these may need to be downloaded on first execution, yes???!
+source '/home/admin/MyScripts/script/helpers/vars.bash'
+source '/home/admin/MyScripts/script/helpers/init.bash'
+
+vars_init
+script_init
+
+# VARIABLES
+version="0.7.0"
 quiet=0
 verbose=0
 force=0
 strict=1
 debug=0
 
-#   ERROR CODES - not in use so far
-EXIT_SUCCESS=0
-EXIT_FAILURE=1
-EXIT_ERROR=2
-EXIT_BUG=10
-
-# ######################################################################################
-#   functions -> these need to go somehere else sometime, as they are helpers...
-
-# DESC: debugging options
-# ARGS: Flags debug, strict set
-# OUTS: debugging info
-# INFO: is called right away
-
-flags_init() {
-	local quiet=${quiet}
-	local verbose=${verbose}
-	local force=${force}
-	local strict=${strict}
-	local debug=${debug}
-
-	# enforce running in debug mode
-	# one `COULD` build a unset -x functionality also...
-	if [[ "${debug}" == "1" ]]; then
-		set -x
-	fi
-	# Exit on empty variable
-	if [[ "${strict}" == "1" ]]; then
-		set -o nounset
-	fi
-
-	# Bash will remember & return the highest exitcode in a chain of pipes.
-	# This way you can catch the error in case mysqldump fails in `mysqldump |gzip`, for example.
-	set -o pipefail
-
-	# Exit on error. Append '||true' when you run the script if you expect an error.
-	set -o errexit
-}
-flags_init
-
-# DESC: Initialise colour variables
-# ARGS: None
-# OUTS: Read-only variables with color codes
-
-function color_init() {
-	readonly RED='\033[0;31m'
-	readonly YELLOW='\e[33m'
-	readonly NOC='\033[0m'
-	readonly BLUE='\e[34m'
-	readonly GREEN='\e[0;32m'
-}
-
-# DESC: Generic script initialisation
-# ARGS: $@ (optional): Arguments provided to the script
-# OUTS: $exec_path: The current working directory when the script was run
-#       $script_path: The full path to the script
-#       $script_dir: The directory path of the script
-#       $script_name: The file name of the script
-
-function script_init() {
-	local exec_path="$PWD"
-	readonly script_path="${BASH_SOURCE[1]}"
-	readonly script_dir="$(dirname "$script_path")"
-	readonly script_name="$(basename "$script_path")"
-	readonly script_params="$*"
-}
-
-# DESC: print usage information
-# ARGS: None
-# OUTS: None
-# NOTE: must be customised to script to provide sensible info, duh.
 
 usage() {
 
