4e481b4f5f38463da70cf1b3ed378e22948ca7b1
rogrwhitakr <bennoosterholt@gmail.com>
2018-12-12 17:16:58 +0100

    test

--- coreutils/printf.bash
+++ coreutils/printf.bash
@@ -2,7 +2,7 @@
 
 source  /home/admin/MyScripts/script/helpers/init.bash
 # strftime -> %F -> y-m-d %R 24 hour notation of hours and minutes
-debug=1
+debug=0
 flags_init
 
 printf "This is week %(%U/%Y)T.\n" -1
@@ -13,3 +13,6 @@ printf '%(%F %T)T :: %s\n' -1 "thest inpot"
 
 # okay, wrapping
 printf '%(%F %T)T :: %s\n' -1 "thest inpot"
+
+# simple print
+printf "dat gay ninj\nsomewhat"
