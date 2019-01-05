cb5d328d54cdd3d1ebc2bb99e215e7b8b21444ab
rogrwhitakr <bennoosterholt@gmail.com>
2019-01-05 20:12:09 +0100

    what

--- coreutils/mounting-filesystems.bash
+++ coreutils/mounting-filesystems.bash
@@ -4,3 +4,4 @@
 # these two mount the windows shares
 sudo mount -t cifs -o user=JohnReese,password=***,domain=WORKGROUP //192.168.0.6/Musik /home/admin/Musik
 sudo mount -t cifs -o user=JohnReese,password=***,domain=WORKGROUP //192.168.0.6/E$ /home/admin/Videos
+
