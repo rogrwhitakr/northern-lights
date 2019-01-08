47698c526932e71d7434a39d00864194b7d22759
rogrwhitakr <bennoosterholt@gmail.com>
2019-01-08 17:12:53 +0100

    added xargs

--- coreutils/find.bash
+++ coreutils/find.bash
@@ -31,3 +31,27 @@ sudo find . -name data_backup.log -size +1M -not -size +2M
 
 # remove all retry files from current dir
 find . -name *.retry -print -delete
+
+# the terminating zeichen determines -exec command behaviour:
+# ; = exec command is repeated for every find  
+#   file [...] file1 
+#   file [...] file2
+#   ... 
+# + = exec command is repeated once for all finds  
+#   file [...] file1 file2 ...
+
+find . -name *.png -exec file -b {} \; | grep "800 x"
+find . -name *.png -exec file -b {} \+ | grep "800 x"
+
+# xargs
+#   is for performance, as it runs a lot faster then the find built-in
+#   most important flags
+#   -n , --max-args (if left out defaults to 5000)
+#   -0 , -- null (inputs are NULL separated (ASCII CODE \000))
+# use in combination! -> find .... print0 | xargs --null 
+
+find . -name *.png -print0 | xargs --null --max-args=1 file -b
+# -n1 Tells xarg to execute the command [cmd] with only one argument 
+# (In this case only one file found by find). 
+# This is equal to:
+#       find . -name *.png -exec file -b {} \;
