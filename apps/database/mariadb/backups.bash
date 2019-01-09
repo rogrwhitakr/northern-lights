b909681bffed0424814465703b3bcf6bc5efa848
rogrwhitakr <bennoosterholt@gmail.com>
2019-01-09 07:34:43 +0100

    some work

--- apps/database/mariadb/backups.bash
+++ apps/database/mariadb/backups.bash
@@ -1,16 +1,38 @@
+#! /usr/bin/env bash
 # todo:
 
+set -o nounset
+set -x
 # 1. identify the databases
 # 2. loop through those, making a backup
 # 3. zip those backups using borg maybe
 # 4. all systemd run
 
-# get a defaults file and coly to home dir, location may not be final
-find / -name mysql-clients.cnf -print -exec cp {} ~/ \; 2>/dev/null
+db_user="root"
+db_password="vasques"
 
+# get a defaults file and coly to home dir, location may not be final
+find /etc -name mysql-clients.cnf -exec install {} --target-directory ~/ --owner "$(id -un)" --group "$(id -un)" \; 2>/dev/null
+
+echo "got through find"
+
+# modify utility
+
+sed --in-place '/^user/d' ~/mysql-clients.cnf
+sed --in-place '/^password/d' ~/mysql-clients.cnf
+
+echo deletion okay
+read
+
+#sed --in-place 's/^[mysqldump]$/[mysqldump] \nuser=${db_user}\npassword=${db_password}/g' ~/mysql-clients.cnf
+sed --in-place 's/^[mysqlshow]$/[mysqlshow] \nuser=aurora\npassword=aurora/g' ~/mysql-clients.cnf
+
+cat ~/mysql-clients.cnf
+read
 # own it, this unfortungately does not work without sudo
 # maybe "install" ????
-sudo chown "$(id -un)"."$(id -un)" mysql-clients.cnf 
+# sudo chown "$(id -un)"."$(id -un)" mysql-clients.cnf 
+# install works! no need to own and run privileged
 
 # create a backup dir
 backup_dir=~/backups
