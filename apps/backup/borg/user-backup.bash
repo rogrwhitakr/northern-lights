119d70e99c64817bb83afe90f65b121b02483b8a
benno <bennoosterholt@gmail.com>
2019-05-06 18:14:56 +0200

    ok

--- apps/backup/borg/user-backup.bash
+++ apps/backup/borg/user-backup.bash
@@ -4,3 +4,5 @@ echo "I HAVE BEEN EXPECTING YOU, MISTER BOND!!!"
 echo "VERY WELL....!" 
 
 exit 0
+
+# cp -f ~/Scripts/apps/backup/borg/user-backup.* user/ && chmod u+x user/*.bash && systemctl --user daemon-reload
