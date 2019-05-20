6053ba41d643903d36f3213447074dc46c0cf5d5
benno <bennoosterholt@gmail.com>
2019-05-20 20:22:49 +0200

    tmp dir, does not seem to work

--- systemd/user-unit-file/user-backup.bash
+++ systemd/user-unit-file/user-backup.bash
@@ -3,9 +3,9 @@
 
 echo "I HAVE BEEN EXPECTING YOU, MISTER BOND!!!"
 echo "VERY WELL....!" 
-mktemp -p "${BASHPID}"
-mktemp "${BASHPID}"/"$(date "+%Y-%m-%d_%H:%M:%S")"
-
+export TMPDIR="/tmp"
+mktemp "$(date "+%Y-%m-%d_%H:%M:%S")".XXXXXXXX
+mktemp "${BASHPID}".XXXXXXX
 exit 0
 
 # cp -f ~/Scripts/apps/backup/borg/user-backup.* user/ && chmod u+x user/*.bash && systemctl --user daemon-reload
