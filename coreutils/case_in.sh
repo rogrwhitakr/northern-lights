6c1efdcada44cd1c01f68bb1a58d74f8907b2ecf
benno <bennoosterholt@gmail.com>
2019-06-22 21:11:18 +0200

    added acolumn explaniner

--- coreutils/case_in.sh
+++ coreutils/case_in.sh
@@ -16,3 +16,24 @@ case "${selection}" in
     echo "Unexpected expression '${selection}'"
     ;;
 esac
+
+ read -p "Name a Star Trek character: " CHAR
+
+ case $CHAR in
+
+   "Seven of Nine" | Neelix | Chokotay | Tuvok | Janeway )
+       echo "$CHAR was in Star Trek Voyager"
+       ;;&
+
+   Archer | Phlox | Tpol | Tucker )
+       echo "$CHAR was in Star Trek Enterprise"
+       ;;&
+   Odo | Sisko | Dax | Worf | Quark )
+       echo "$CHAR was in Star Trek Deep Space Nine"
+       ;;&
+   Worf | Data | Riker | Picard )
+       echo "$CHAR was in Star Trek The Next Generation"
+       ;;
+   *) echo "$CHAR is not in this script." 
+       ;;
+ esac
