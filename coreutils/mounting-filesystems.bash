45b461da5dd2ea924fd391ded88a438468587bea
benno <benno@serenity.northern-lights.one>
2019-06-24 07:20:26 +0200

    added a header

--- coreutils/mounting-filesystems.bash
+++ coreutils/mounting-filesystems.bash
@@ -1,5 +1,7 @@
+###############################################################################
 # mount
 # mount is somewhat legacy. man page says: use findmnt, especially for scripts!
+###############################################################################
 
 # these two mount the windows shares
 sudo mount -t cifs -o user=JohnReese,password=***,domain=WORKGROUP //192.168.0.6/Musik /home/admin/Musik
