ef3a02499355246ca18cb0977621626ace42113e
Malcolm Reynolds <bennoosterholt@gmail.com>
2019-07-16 07:22:07 +0200

    works now

--- conf/network/ssh/create-ssh-profile.sh
+++ conf/network/ssh/create-ssh-profile.sh
@@ -1,10 +1,12 @@
 #! /usr/bin/env bash
 
+trap echo "something failed. exit by trap" INT TERM EXIT 
+
 # make sure this is not run by the root user
-if [ ${id -u} == 0 ]; then
-    printf "creating ssh config for user ${id -un}"
+if [ "$(id -u)" != 0 ]; then
+    echo -e "creating ssh config for user $(id -un)"
 else
-    printf "this script must not be run as root!\nExiting"
+    echo -e "this script must not be run as root!\nExiting"
     exit 1
 fi
 
@@ -17,11 +19,26 @@ if [ ! -d ~/.ssh ]; then
 fi    
 
 # populate the config file
-curl -o ~/.ssh/config.github 'https://raw.githubusercontent.com/rogrwhitakr/northern-lights/master/conf/dotfiles/.ssh.northernlights.config'
+# first we look locally
 
-# own the config file
-chown ${id -un} ~/.ssh/config
+# script directory also contains a default sshconfig file
+__dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+__file="ssh.default.config"
+echo -e "copying file ${__dir}/${__file} to ssh directory"
+cp "${__dir}/${__file}" ~/.ssh/config
+
+# if copy fails we source from github
+if [[ $? != 0 ]]; then
+    echo -e "copy process failed. curling from github"
+    curl -o ~/.ssh/config.github 'https://raw.githubusercontent.com/rogrwhitakr/northern-lights/master/conf/network/ssh/ssh.default.config'
+fi
+# own the config file, set right params
+chown $(id -un) ~/.ssh/config
 chmod 600 ~/.ssh/config
 
 # if all else fails, recheck this last one
+# leaving this one in place for its unnecessary complexity and convolutedness...
 find ~/.ssh/config ! -perm 600 -exec chmod 600  {} \;
+
+echo -e "ssh config now in place" 
+exit 0
