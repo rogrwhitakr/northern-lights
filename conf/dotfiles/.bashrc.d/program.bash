782cd4a7d95a6021e3eacf09c3e71a6205943b8d
Malcolm Reynolds <bennoosterholt@gmail.com>
2019-07-22 08:07:35 +0200

    removed static commands

--- conf/dotfiles/.bashrc.d/program.bash
+++ conf/dotfiles/.bashrc.d/program.bash
@@ -6,11 +6,6 @@ alias enabled='systemctl is-enabled "$@"'
 # update the system
 alias update="sudo dnf update -y"
 
-# git
-alias gits="ret=$(pwd) && cd ~/MyScripts && git status && cd $ret"
-alias pull_nl='cd ~/MyScripts && git pull northern-lights'
-alias push_nl='cd ~/MyScripts && git push northern-lights'
-
 # ansible
 # local vm status
 # this is somewhat problematic as NO cheks take place beforehand....
