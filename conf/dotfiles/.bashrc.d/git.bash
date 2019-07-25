d64f0396540a9ddc8e371551926fb6104108dff6
Malcolm Reynolds <bennoosterholt@gmail.com>
2019-07-25 06:52:32 +0200

    replaced the hardcoded stuff with functions

--- conf/dotfiles/.bashrc.d/git.bash
+++ conf/dotfiles/.bashrc.d/git.bash
@@ -1,22 +1,131 @@
-#!/usr/bin/env bash
+gitfinish() {
 
-export gits='git status';
+	# DESC: it seems it is not possible to use trap locally, i.e. limited to a specific function
+	#       As these functions are sourced by bashrc, whenever I get a return error on ANY command,
+	#		the traps are called. maybe adding to function limits this behaviour???
+	# ARGS: directory
+	# OUTS: return
 
-# shorthand for the northern-lights repository
-export pull_nl="git pull northern-lights --rebase";
-export push_nl='git push northern-lights master';
+	trap "echo 'directory is not a git repository, exiting' && cd ${current} && return 1" ERR
 
-# shorthand for the ambient-lights repository
-export pull_nl="git pull origin master --rebase";
-export push_nl='git push origin master';
+	echo "illegal"
+}
+# todo extract func trion for path setting
+set_git_path() {
 
-# shorthand for the docker repository
-export pull_nl="git pull cube-lights --rebase";
-export push_nl='git push cube-lights master';
+	# DESC: set repository directory
+	#       and exits on wrong path
+	# ARGS: repository directory
+	# OUTS: return code
 
-check_repository() {
-	local repository='$1'
-	if [[ -z "$@" ]]; then
-		echo -e "no input given to append to ${repository}"
-		break
+	# vars
+	local repository="$1"
+	local current="$(/usr/bin/pwd)"
+
+	# we trap errors and such
+	#	trap "echo 'directory is not a git repository, exiting' && cd ${current} && return 1" ERR
+
+	# we switch to repository directory, if not give3n we assume current directory
+	cd "${repository:-$current}"
+	git status 2 &>1 >/dev/null || return $?
+}
+
+gs() {
+
+	# DESC: set repository directory
+	#       gets the status of the repository
+	# ARGS: repository directory
+	# OUTS: git status
+
+	# vars
+	local repository="$1"
+	local current="$(/usr/bin/pwd)"
+
+	# we trap errors and such
+	#	trap "echo 'No repository directory given' && cd ${current}" ERR
+
+	# we go to provide directory, if not given we use current
+	cd "${repository:-$current}"
+	git status 2>/dev/null
+
+	# return to parent
+	cd "${current}"
+}
+
+gc() {
+
+	# DESC: set repository directory
+	#       adds and commits the given changes
+	# ARGS: repository directory
+	# OUTS: committed files
+
+	# vars
+	local repository="$1"
+	local current="$(/usr/bin/pwd)"
+
+	# we trap errors and such
+	#	trap "echo 'git commit failed, exiting' && cd ${current}" ERR
+
+	# we switch to repository directory, if not give3n we assume current directory
+	cd "${repository:-$current}"
+
+	# validate git directory
+	if [ -n "$(git status --porcelain)" ]; then
+		echo "there are changes:"
+		git diff --minimal
+
+		# read a commit message
+		# to do limit amount of chars
+		read -rp $'Enter commit message here: ' -e commit_message
+
+		# commit
+		git add .
+		git commit -m "${commit_message}"
+
+	else
+		echo "no changes"
 	fi
+
+	# return to source directory
+	cd "${current}"
+}
+
+gpull() {
+
+	# DESC: set repository directory
+	#       adds and commits the given changes
+	# ARGS: repository directory
+	# OUTS: committed files
+
+	# vars
+	local repository="$1"
+	local current="$(/usr/bin/pwd)"
+
+	# we trap errors and such
+	#	trap "echo 'git pull rebase failed, exiting' " ERR INT TERM EXIT
+
+	# we set the path
+	set_git_path "${repository}"
+
+	# we assume changes have happenend on the remote
+	# rebase
+	git pull $(git remote show | head -n1) master --rebase
+}
+
+gpush() {
+
+	# DESC: set repository directory
+	#       adds and commits the given changes
+	# ARGS: repository directory
+	# OUTS: committed files
+
+	# vars
+	local repository="$1"
+	local current="$(/usr/bin/pwd)"
+
+	# we set the path
+	set_git_path "${repository}"
+
+	# we assume remote changes have been pulled already
+	git push $(git remote show | head -n1) master
+}
