27aa66491cb22228baec802bfb0306b17b185b66
Malcolm Reynolds <bennoosterholt@gmail.com>
2019-07-26 07:22:43 +0200

    addses, corrections

---
+++ apps/packaging/rpm-apt-dnf-ostree/packet-manager-repositories.bash
@@ -0,0 +1,7 @@
+#! /usr/bin/env bash
+
+# viewing repositories that dnf knows
+sudo dnf repolist
+
+# location of these
+/etc/yum.d/...
--- coreutils/lsof.bash
+++ coreutils/lsof.bash
@@ -1,3 +1,12 @@
 
 # lsof - list open files
 lsof -i
+
+# from reddit
+# Kill all processes listening on a certain port
+
+port=8080
+lsof -i :"${port}" | tail -n+2 | awk '{ print $2 }' | xargs kill
+
+# also, does the same thing, more cleanly
+fuser -k -SIGTERM "${port}"/tcp
