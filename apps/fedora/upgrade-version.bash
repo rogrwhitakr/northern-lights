ad20358f28d37a1141633446f7488c417a89a89c
rogrwhitakr <bennoosterholt@gmail.com>
2019-08-18 15:06:47 +0200

    added line with curl issue

--- apps/fedora/upgrade-version.bash
+++ apps/fedora/upgrade-version.bash
@@ -7,5 +7,8 @@ sudo dnf install dnf-plugin-system-upgrade -y
 # download all new things
 sudo dnf system-upgrade download --releasever=30 --allowerasing -y
 
+# in case of curl error
+sudo dnf system-upgrade download --releasever=30 --allowerasing -y --nogpgcheck
+
 # do the update
 sudo dnf system-upgrade reboot
