b156235ef407a5028abab5dfd01d58ef3ef50fcd
Malcolm Reynolds <bennoosterholt@gmail.com>
2019-08-26 07:03:07 +0200

    put into hold pattern, need work sometime

--- apps/packaging/flatpak/flatpack-builder.bash
+++ apps/packaging/flatpak/flatpack-builder.bash
@@ -4,3 +4,4 @@
 # use the pbf combiner script to create a flatpak with
 # https://blogs.gnome.org/alexl/2017/02/10/maintaining-a-flatpak-repository/
 
+# http://docs.flatpak.org/en/latest/flatpak-builder.html
---
+++ apps/packaging/flatpak/flatpak-repository.bash
@@ -0,0 +1,2 @@
+# Maintaining a flatpak repository
+# https://blogs.gnome.org/alexl/2017/02/10/maintaining-a-flatpak-repository/
