943acd53b4d87ed1cfc94c2fad444533bcea3f88
Malcolm Reynolds <bennoosterholt@gmail.com>
2019-09-17 07:31:32 +0200

    added bash command line thingy

---
+++ coreutils/bash-commandline-params.bash
@@ -0,0 +1,9 @@
+#
+# bash command line stuffs
+#
+
+
+# the ‘Last Argument’ One
+# substitute in the last argument of the last command into your line. 
+
+$!
---
+++ coreutils/readline.bash
@@ -0,0 +1,4 @@
+# Readline explainer
+# Readline reads a ~/.inputrc file on startup that can be used to configure various options and key bindings
+#
+#
