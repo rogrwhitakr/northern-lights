2fd65dee3bbcb5b5feb9990247a553dd4c802e9c
Malcolm Reynolds <bennoosterholt@gmail.com>
2019-09-17 07:34:56 +0200

    addses

--- coreutils/bash-commandline-params.bash
+++ coreutils/bash-commandline-params.bash
@@ -3,7 +3,13 @@
 #
 
 
-# the ‘Last Argument’ One
-# substitute in the last argument of the last command into your line. 
+# substitute in the last argument of the last command into your line.
+!$
 
-$!
+# subsitute any command in order
+!:n
+
+# if command is something like, you can rearrange like so
+tar -cvf archive.tar <decompressed_archive>
+
+!:0 !:1 !:3 !:2
