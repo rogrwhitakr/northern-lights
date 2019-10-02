53158be3531d31bb7544ab516a2597924fcdd5dc
Malcolm Reynolds <bennoosterholt@gmail.com>
2019-10-02 06:50:55 +0200

    completed the definition

--- apps/database/_data/price-calculator.sql
+++ apps/database/_data/price-calculator.sql
@@ -35,22 +35,31 @@ create table if not exists price_list ( _price_list_id serial unique primary key
 item_name char(255) not null,
 item_price int not null );
 
-create table if not exists price_list_description ( _price_list_description_id serial unique primary key,
-item_description char(1000) null,
-item_description_id int not null references price_list(_price_list_id) on
-update
-	cascade on
-	delete
-		cascade,
-		description_language lang not null );
+create table if not exists price_list_description ( 
+	_price_list_description_id 	serial unique primary key,
+	item_description 			char(1000) null,
+	item_description_id 		int not null references price_list(_price_list_id) on update cascade on delete cascade,
+	description_language 		lang not null
+);
 
-create table if not exists item_list ( _item_list_id serial unique primary key,
-item_id int not null references price_list(_price_list_id) on
-update
-	restrict,
-	amount int not null );
+create table if not exists item_list ( 
+	_item_list_id 	serial unique primary key,
+	item_id 		int not null references price_list(_price_list_id) on update restrict,
+	offer_id 		int not null references prospect_offer(_prospect_offer_id) on update cascade on delete cascade,
+	amount 			int not null
+);
 
-create table if not exists prospect_offer ( _prospect_offer_id serial unique primary key,
-offer_title char(255) null,
-offer_language lang not null,
-reference_id int not null)
+create table if not exists prospect_offer ( 
+	_prospect_offer_id 	serial unique primary key,
+	prospect	 		int not null references prospect(_prospect_id) on update cascade on delete cascade,
+	reference_id 		char(255) not null, -- external ID of the offer
+	offer_title 		char(255) null,
+	offer_language 		lang not null,
+);
+
+-- in-place 
+-- alter table prospect_offer add column prospect int not null references prospect(_prospect_id) on update cascade on delete cascade;
+-- alter table prospect_offer modify column reference_id type char(255) not null;
+
+select * from price_list
+select * from prospect_offer
