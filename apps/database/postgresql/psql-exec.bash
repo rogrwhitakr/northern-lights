dd7116b824e8a448d39361cee65bb7b34ca527d9
benno <bennoosterholt@gmail.com>
2019-10-05 14:37:49 +0200

    dont werk yet

--- apps/database/postgresql/psql-exec.bash
+++ apps/database/postgresql/psql-exec.bash
@@ -10,3 +10,12 @@ pg_dump --host=sql --user=postgres redmine > redmine.sql --verbose
 
 # setting evironment variable for automation
 PGPASSWORD=xy
+
+# https://www.postgresql.org/docs/12/libpq-envars.html
+
+PGHOST behaves the same as the host connection parameter.
+PGHOSTADDR behaves the same as the hostaddr connection parameter. This can be set instead of or in addition to PGHOST to avoid DNS lookup overhead.
+PGPORT behaves the same as the port connection parameter.
+PGDATABASE behaves the same as the dbname connection parameter.
+PGUSER behaves the same as the user connection parameter.
+PGPASSWORD behaves the same as the password connection parameter
