573bcff4fd3f3c00785745f2eed7356b7eb9e5a9
HaroldFinch <finch@northern-lights.one>
2020-11-18 11:23:46 +0100

    add rsync explainer -> i always used scp

---
+++ coreutils/rsync.bash
@@ -0,0 +1,7 @@
+#! /usr/bin/env bash
+
+Local Sync: # rsync {options} {Source} {Destination}
+
+Remote Sync pull: # rsync {options}  <User_Name>@<Remote-Host>:<Source-File-Dir>  <Destination>
+
+Remote Sync Push: # rsync  <Options>  <Source-Files-Dir>   <User_Name>@<Remote-Host>:<Destination>
