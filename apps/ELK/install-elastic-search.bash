9e7c1ba5cfa465a9fe5f992e2b77a5b98950b71b
HaroldFinch <finch@northern-lights.one>
2020-11-19 12:26:51 +0100

    move and add for clarity

--- apps/ELK/install-elastic-search.bash
+++ apps/ELK/install-elastic-search.bash
@@ -1,8 +1,36 @@
 #! /usr/bin/env bash
 
-# how to install elastic-search of fedora
+# functions
+print() {
+	case "${1^^}" in
+	RED)
+		printf '\033[0;31m%s\033[0m\n' "${2}"
+		;;
+	YELLOW)
+		printf '\e[33m%s\033[0m\n' "${2}"
+		;;
+	GREEN)
+		printf '\e[0;32m%s\033[0m\n' "${2}"
+		;;
+	LINE)
+		separator="-"            # separator default
+		line="["                 # adding to a "line" variable
+		term_size="$(tput cols)" # get number of columns
+		if ([[ ! -z "$2" ]] && [[ "${#2}" == 1 ]]); then # set custom the separator (length must be 1)
+			separator="${2}"
+		fi
+		for ((i = 1; i <= "${term_size}-2"; i++)); do # make the line
+			line+="${separator}"
+		done
+		line+="]"
+		printf '%s\n' "${line}" # regurgitate to terminal
+		;;
+	*)
+		printf '\033[0m%s\033[0m\n' "${1}"
+		;;
+	esac
+}
 
-# Step 1: installing requires escalation priviledge -> thus running as root
 rootcheck() {
 	if [ "$(id -u)" != "0" ]; then
 		echo "This script must be run as root" 1>&2
@@ -10,9 +38,17 @@ rootcheck() {
 	fi
 }
 
+#############################
+# exec
+
+print line
+print green "# install elasticsearch on Fedora: $hostname"
+print line
+
+print red "# Step 1: installing requires escalation priviledge -> thus running as root"
 rootcheck
 
-# Step 2: Add ELK repository
+print red "# Step 2: Add ELK repository"
 cat <<EOF | tee /etc/yum.repos.d/elasticsearch.repo
 [elasticsearch-7.x]
 name=Elasticsearch repository for 7.x packages
@@ -20,42 +56,47 @@ baseurl=https://artifacts.elastic.co/packages/7.x/yum
 gpgcheck=1
 gpgkey=https://artifacts.elastic.co/GPG-KEY-elasticsearch
 enabled=1
-autorefresh=1
 type=rpm-md
 EOF
 
-# After adding the repo, import GPG key:
+print red "# import GPG key"
 rpm --import https://artifacts.elastic.co/GPG-KEY-elasticsearch
 
-# Clear and update your dnf package index.
+print line
+print red "# Clear and update dnf package index"
 dnf clean all -v
 dnf makecache
 
-# Step 3: Install Elasticsearch, java
+print yellow "# Step 3: Install Elasticsearch, java"
 dnf -y install elasticsearch java-openjdk-devel java-openjdk
 
-# service
-systemctl enable --now elasticsearch.service 
-
-# Step 4: Install and Configure Kibana
+print yellow "# Step 4: Install Kibana"
 dnf -y install kibana
 
-# service
-systemctl enable --now kibana
+print yellow "# Step 5: install logstash" 
+dnf -y install logstash
+
+print yellow "# Step 6: Install other ELK tools"
+# Filebeat: Lightweight Shipper for Logs. It helps you keep the simple things simple by offering a lightweight way to forward and centralize logs and files
+# Metricbeat: Collect metrics from your systems and services. From CPU to memory, Redis to NGINX, and much more, Metricbeat is a lightweight way to send system and service statistics.
+# Packetbeat: Lightweight Shipper for Network Data
+# Heartbeat: Lightweight Shipper for Uptime Monitoring. It helps you monitor services for their availability with active probing
+# Auditbeat: Lightweight shipper that helps you audit the activities of users and processes on your systems
+dnf install -y filebeat auditbeat metricbeat packetbeat heartbeat-elastic
+
+print line
+print red "# Step 7: add firewall rules, unit files and rules"
 
 # firewall
 firewall-cmd --add-port=5601/tcp --permanent
 firewall-cmd --add-port=9200/tcp --permanent
 firewall-cmd --reload
 
-# Step 5: logstash 
-dnf -y install logstash
+# services
+systemctl enable --now kibana.service
+systemctl enable --now elasticsearch.service 
+systemctl enable --now logstash.service 
 
-# Step 6: Install other ELK tools – Bonus
-# Filebeat: Lightweight Shipper for Logs. It helps you keep the simple things simple by offering a lightweight way to forward and centralize logs and files
-# Metricbeat: Collect metrics from your systems and services. From CPU to memory, Redis to NGINX, and much more, Metricbeat is a lightweight way to send system and service statistics.
-# Packetbeat: Lightweight Shipper for Network Data
-# Heartbeat: Lightweight Shipper for Uptime Monitoring. It helps you monitor services for their availability with active probing
-# Auditbeat: Lightweight shipper that helps you audit the activities of users and processes on your systems
-
-dnf install -y filebeat auditbeat metricbeat packetbeat heartbeat-elastic
+print line
+print green "# Complete"
+print line
