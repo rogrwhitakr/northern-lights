ec7f84c6b8e1187add47f8929ffdfc7e5e7bf80c
HaroldFinch <finch@northern-lights.one>
2020-11-24 22:58:15 +0100

    add a timeline printer

--- script/helpers/printer.bash
+++ script/helpers/printer.bash
@@ -50,6 +50,19 @@ print() {
 		logline+="]"
 		printf '%s\n' "${logline}" # regurgitate to terminal
 		;;
+	TIMELINE)
+		separator="-"            # separator default
+		line="[---[$(date +"%Y-%m-%d %H:%M:%S")]"                 # adding to a "line" variable
+		term_size="$(tput cols)" # get number of columns
+		if ([[ ! -z "$2" ]] && [[ "${#2}" == 1 ]]); then # set custom the separator (length must be 1)
+			separator="${2}"
+		fi
+		for ((i = 1; i <= "${term_size}-26"; i++)); do # make the line
+			line+="${separator}"
+		done
+		line+="]"
+		printf '%s\n' "${line}" # regurgitate to terminal
+		;;	
 	LINE)
 		separator="-"            # separator default
 		line="["                 # adding to a "line" variable
