412fe625108ec91373ac9ae329f09ba693dfb613
HaroldFinch <finch@northern-lights.one>
2020-12-11 23:05:41 +0100

    moved some stuff around

---
+++ coreutils/usermod.bash
@@ -0,0 +1,5 @@
+#! /usr/bin/env bash
+
+# add user to sublemental group like libvirt / kvm / qemu
+sudo usermod -a -G libvirt admin
+
--- script/utils/positional-parameters.bash
+++ script/utils/positional-parameters.bash
@@ -1,4 +1,4 @@
-#! /usr/bin/env sh
+#! /usr/bin/env bash
 
 parameter_set(){
     echo "$1"
