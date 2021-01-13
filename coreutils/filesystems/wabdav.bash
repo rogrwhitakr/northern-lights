70cce0fcc7bcb0daa2b2a7ebd66e2df3b7e94980
HankScorpio <bennoosterholt@gmail.com>
2021-01-13 15:32:57 +0100

    k

--- coreutils/filesystems/wabdav.bash
+++ coreutils/filesystems/wabdav.bash
@@ -5,3 +5,12 @@ dnf install davfs2
 
 # mount the filesystem
 sudo mount -t davfs https://<url> /mnt
+
+# mount a nextcloud instance directory
+davs://storage.northern-lights.one/nextcloud/remote.php/dav/files/USER/
+
+# dolphin
+webdav://example.com/nextcloud/remote.php/dav/files/USERNAME/
+
+# nautilus
+davs://example.com/nextcloud/remote.php/dav/files/USERNAME/
